3222e087302679af7c0d210ec4d1092274fafa5c
Oscar Leal <oscarleal234@gmail.com>
2024-05-17 15:53:33 -0500

    added some padding to about

--- app/componenets/aboutpage/mikeinfo.tsx
+++ app/componenets/aboutpage/mikeinfo.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 import { Image } from '@nextui-org/react';
 const MikeInfo = () => {
   return (
-    <div className="flex flex-row justify-evenly flex-wrap lg:flex-nowrap">
+    <div className="flex flex-row justify-evenly flex-wrap lg:flex-nowrap m-10">
       {' '}
+      <h2>Michael Estrada</h2>
       <Image
         isBlurred
         width={400}
         src="https://mikessite.s3.us-east-2.amazonaws.com/MikePic.JPEG"
         alt="Mike Bartending"
-        className="pt-3"
+        className="pt-3 lg:m-10"
       />
-      <h2>Michael Estrada</h2>
     </div>
   );
 };
--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from '@nextui-org/react';
 
 export default function Loading() {
   return (
-    <Spinner className="flex justify-center" color="success">
+    <Spinner className="flex justify-center text-4xl" color="default">
       Loading
     </Spinner>
   );
