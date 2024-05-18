c5b8c85d5411d0266f3903060b3cb1a860444d6b
Oscar Leal <oscarleal234@gmail.com>
2024-05-18 17:08:00 -0500

    added gallery

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import MikeInfo from '../componenets/aboutpage/mikeinfo';
+import Gallery from '../componenets/aboutpage/gallery';
+import AboutButton from '../componenets/aboutpage/aboutbutton';
 export default async function About() {
   return (
     <div>
       <MikeInfo />
+      <Gallery />
+      <AboutButton />
     </div>
   );
 }
---
+++ app/componenets/aboutpage/aboutbutton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button } from '@nextui-org/react';
+
+import Link from 'next/link';
+import { Image } from '@nextui-org/react';
+const AboutButton = () => {
+  return (
+    <div className="flex justify-center">
+      <Link href={'/contact'}>
+        <Button
+          className="bg-[#FFFFF0] text-black hover:bg-[#DC143C] m-10"
+          variant="shadow"
+        >
+          Book Now
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default AboutButton;
