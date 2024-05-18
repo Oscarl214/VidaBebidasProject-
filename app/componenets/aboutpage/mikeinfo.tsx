6083a57f80c47fd6d0170f3cb5f399c7b814ecfa
Oscar Leal <oscarleal234@gmail.com>
2024-05-18 17:28:41 -0500

    added UI swipe icon

--- app/componenets/aboutpage/mikeinfo.tsx
+++ app/componenets/aboutpage/mikeinfo.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const MikeInfo = () => {
   return (
-    <div className="flex flex-row justify-evenly flex-wrap  m-10 border-b-1 border-[#FFFFF0]">
-      <div className="flex flex-col gap-5">
+    <div className="flex flex-row justify-evenly flex-wrap  m-10 ">
+      <div className="flex flex-col gap-5 border-b-1 border-[#FFFFF0]">
         <h2 className="text-4xl text-center text-[#FFD700]">Michael Estrada</h2>
         <p className="text-wrap text-center lg:text-2xl leading-7 m-3">
           Michael is a dedicated and professional bartender who brings
@@ -16,6 +16,9 @@ const MikeInfo = () => {
           professionalism makes VidaBebidasProject a trusted choice for any
           occasion.
         </p>
+      </div>
+      <div>
+        <span className="icon-[material-symbols--swipe-outline] text-3xl mt-10 mb-0"></span>
       </div>
     </div>
   );
