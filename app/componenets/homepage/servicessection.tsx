1b00c85497ec38acb246913f9bf7c76dd9d366b1
Oscar Leal <oscarleal234@gmail.com>
2024-05-18 19:02:36 -0500

    added packages data

--- app/componenets/homepage/servicessection.tsx
+++ app/componenets/homepage/servicessection.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
 import { Card, CardBody, Image, Button, Slider } from '@nextui-org/react';
+import PackageData from '../../packagesData.json';
 
 const ServicesSection = () => {
   return (
     <div>
       <h2 className="text-5xl text-[#FFFFF0] text-center m-5">Packages</h2>
       <div>
-        {' '}
-        <Card
-          isBlurred
-          className="border-none bg-background/60 dark:bg-default-100/50 max-w-[610px]"
-          shadow="sm"
-        >
-          <CardBody>
-            <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
-              <div className="relative col-span-6 md:col-span-4">
-                <Image
-                  alt="Album cover"
-                  className="object-cover"
-                  height={200}
-                  shadow="md"
-                  src="https://images.pexels.com/photos/340996/pexels-photo-340996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  width="100%"
-                />
+        {PackageData.map((pck) => (
+          <Card
+            isBlurred
+            className="border-none bg-background/60 dark:bg-default-100/50 w-65"
+            shadow="sm"
+            key={pck.id}
+          >
+            <CardBody>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-4 items-center justify-center">
+                <div className="relative col-span-1">
+                  <Image
+                    alt="Album cover"
+                    className="object-cover"
+                    height={200}
+                    shadow="md"
+                    src="https://images.pexels.com/photos/340996/pexels-photo-340996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    width="100%"
+                  />
+                </div>
+                <div className="flex flex-col col-span-1 ">
+                  <h1 className="text-4xl text-center">{pck.name}</h1>
+                  <p className="text-start">Price of Package</p>
+                  <ul className="text-start">
+                    <li>Description One</li>
+                  </ul>
+                </div>
               </div>
-            </div>
-          </CardBody>
-        </Card>
+            </CardBody>
+          </Card>
+        ))}
       </div>
     </div>
   );
