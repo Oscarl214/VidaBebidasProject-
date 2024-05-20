b9d500a9174c5f3d46a0f772b3bdcc5db02de5a2
Oscar Leal <oscarleal234@gmail.com>
2024-05-19 21:20:27 -0500

    added book now button to packages

--- app/packages/page.tsx
+++ app/packages/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ServicesSection from '../componenets/packagespage/servicessection';
 import ImportantInfo from '../componenets/packagespage/importantinfo';
+import AboutButton from '../componenets/aboutpage/aboutbutton';
 const Packages = () => {
   return (
     <div>
       <ImportantInfo />
       <ServicesSection />
+      <AboutButton />
     </div>
   );
 };
