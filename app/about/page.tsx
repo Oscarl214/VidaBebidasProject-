a37fd976e8c294c9fd2d6990d317781ecb73af55
Oscar Leal <oscarleal234@gmail.com>
2024-05-20 18:38:07 -0500

    added margin to pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import Gallery from '../componenets/aboutpage/gallery';
 import AboutButton from '../componenets/aboutpage/aboutbutton';
 export default async function About() {
   return (
-    <div>
+    <div className="mt-[150px]">
       <MikeInfo />
       <Gallery />
     </div>
