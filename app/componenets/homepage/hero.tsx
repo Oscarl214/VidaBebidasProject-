dcc4ecb9e82c37656c0a7ce218e66f599bcc22f3
Oscar2145 <74516013+Oscar2145@users.noreply.github.com>
2024-05-21 15:06:50 -0500

    added animation to hero

--- app/componenets/homepage/hero.tsx
+++ app/componenets/homepage/hero.tsx
@@ -5,6 +5,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import HeroBG from '../../../public/HeroBG.jpeg';
 
+
+import { motion } from "framer-motion"
+
 const MainHero = () => {
   return (
     <div className="relative hero min-h-screen lg:min-h-screen md:h-[80vh] h-[30vh]  border border-bottom border-black">
@@ -20,13 +23,22 @@ const MainHero = () => {
 
       <div className="relative z-10 flex flex-col justify-around lg:flex-nowrap flex-wrap">
         <div className="flex flex-col justify-center items-center gap-4 text-[#FFFFF0]">
+          <motion.div     initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}> 
+
           <h1 className="text-[#FFD700] lg:text-7xl font-bold text-3xl z-10">
             VIDABEBIDASPROJECT
           </h1>
           <p className="text-2xl text-center z-10">
             El toque perfecto para tu evento especial.
           </p>
-          <div className="flex flex-row gap-3 z-10">
+          </motion.div>
+      
+        
+          <motion.div className="flex flex-row gap-3 z-10" initial={{ opacity: 0, y: 100 }}   animate={{ opacity: 1, y: 10 }}
+            transition={{ duration: 0.6 }} >
+          
             <Link href="/contact">
               <Button
                 className="bg-[#FFFFF0] rounded-sm text-black hover:bg-[#DC143C]"
@@ -43,8 +55,10 @@ const MainHero = () => {
                 Packages
               </Button>
             </Link>
-          </div>
-          <div className="flex justify-center z-10">
+            </motion.div>
+        
+          <motion.div className="flex justify-center z-10" initial={{ opacity: 0, y: 100 }}   animate={{ opacity: 1, y: 10 }}
+            transition={{ duration: 0.6 }}>
             <Link href="/contact">
               <Button
                 className="bg-[#FFD700] text-black border border-[#DC143C] rounded-sm hover:bg-[#FFFFF0] hover:text-black"
@@ -53,8 +67,10 @@ const MainHero = () => {
                 Leave a Review
               </Button>
             </Link>
-          </div>
+          </motion.div>
+      
         </div>
+        
       </div>
     </div>
   );
