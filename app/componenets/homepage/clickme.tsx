63b5976f765dd8d3b49c90fc5dc32456dc3f4f15
Oscar Leal <oscarleal234@gmail.com>
2024-05-22 13:15:58 -0500

    creating clickMe UI

---
+++ app/componenets/homepage/clickme.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClickMe = () => {
+  return (
+    <div className="absolute top-1 right-10 z-10">
+      <span className="icon-[game-icons--click]"></span>
+    </div>
+  );
+};
+
+export default ClickMe;
