67495db9b26e3fd0a5e734181796f46ee039a886
Oscar Leal <oscarleal234@gmail.com>
2024-05-22 13:58:32 -0500

    added clickMe

--- app/componenets/homepage/clickme.tsx
+++ app/componenets/homepage/clickme.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const ClickMe = () => {
   return (
-    <div className="absolute top-1 right-10 z-10">
-      <span className="icon-[game-icons--click]"></span>
+    <div className="clickmeicon ">
+      <span className="icon-[game-icons--click] text-xl text-white animate-pulse">
+        ffff
+      </span>
     </div>
   );
 };
