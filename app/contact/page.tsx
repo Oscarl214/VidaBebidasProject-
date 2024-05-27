ac93266bc62f2b52ac63c4ba8dcbf951149d27de
Oscar Leal <oscarleal234@gmail.com>
2024-05-26 20:21:08 -0500

    inbedded form in modal

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import GoogleForm from '../componenets/contactpage/form';
 const Contact = () => {
-  return <div className="mt-[100px]">{/* <GoogleForm/> */}</div>;
+  return (
+    <div className="mt-[100px]">
+      {' '}
+      <GoogleForm />{' '}
+    </div>
+  );
 };
 
 export default Contact;
