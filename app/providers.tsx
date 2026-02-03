780a05f222cc1fcec1678545167a0799cc6a5292
Oscarl214 <oscarleal234@gmail.com>
2026-02-03 08:35:26 -0600

    added types

--- app/providers.tsx
+++ app/providers.tsx
@@ -11,7 +11,26 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     OneSignal.init({
       appId: "53d2b6e8-bc6e-480e-8a78-f3a993a3d92a",
       safari_web_id: "web.onesignal.auto.32a023df-3e37-4c19-843f-3978a63a946e",
-      allowLocalhostAsSecureOrigin: true,
+      notifyButton: {
+        enable: true,
+        prenotify: false,
+        showCredit: false,
+        text: {
+          'tip.state.unsubscribed': 'Subscribe to notifications',
+          'tip.state.subscribed': 'You\'re subscribed to notifications',
+          'tip.state.blocked': 'You\'ve blocked notifications',
+          'message.prenotify': 'Click to subscribe to notifications',
+          'message.action.subscribing': 'Subscribing...',
+          'message.action.subscribed': 'Thanks for subscribing!',
+          'message.action.resubscribed': 'You\'re subscribed to notifications',
+          'message.action.unsubscribed': 'You won\'t receive notifications again',
+          'dialog.main.title': 'Manage Site Notifications',
+          'dialog.main.button.subscribe': 'SUBSCRIBE',
+          'dialog.main.button.unsubscribe': 'UNSUBSCRIBE',
+          'dialog.blocked.title': 'Unblock Notifications',
+          'dialog.blocked.message': 'Follow these instructions to allow notifications:'
+        },
+      },
     }).catch(err => console.error('OneSignal init error:', err));
   }, []);
   return (
